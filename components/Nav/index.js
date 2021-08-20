3e24532df9f93b6aa8202327cfe3b7e50d1c0c4a
Damián <dfinkelstein@wolox.com.ar>
2021-08-20 19:42:49 -0300

    added nav bar opaque color when scrolling

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -1,10 +1,31 @@
-import Link from 'next/link'
+import { useEffect, useState } from 'react';
 import Logo from '../../public/assets/ic-liteflix.svg';
 import styles from './styles.module.scss'
+import cn from 'classnames';
+
+const useWindowScroll = () => {
+  const [scroll, setScroll] = useState({ scrollX: null, scrollY: null });
+
+  useEffect(() => {
+    const onScroll = () => {
+      const scroll = { scrollX: window.scrollX, scrollY: window.scrollY };
+      setScroll(scroll);
+    }
+    
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  return scroll;
+}
 
 function Nav({ onOpenSidebar, onAddMovie }) {
+  const { scrollY } = useWindowScroll();
+
+  const shouldChangeNavBackground = () => scrollY > 100;
+
   return (
-    <nav className={styles.nav}>
+    <nav className={cn(styles.nav, { [styles.opaque]: shouldChangeNavBackground() })}>
       <div className={styles.logoContainer}>
         <Logo />
         <button type="button" className={styles.addMovieButton} onClick={onAddMovie}>Agregar Película</button>
