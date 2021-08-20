477bd8b2cf5d3bbcf4404fb3d492ac6f44650648
Damián <dfinkelstein@wolox.com.ar>
2021-08-20 01:44:06 -0300

    click movie when on mobile

--- components/PopularMovies/index.js
+++ components/PopularMovies/index.js
@@ -4,12 +4,14 @@ import Image from 'next/image';
 import PlayButton from '../../public/assets/ic-play-button.svg';
 import cn from 'classnames';
 import { useOnClickOutside } from './hooks';
+import { useEffect } from 'react';
 
 function PopularMovies({ popularMovies, myMovies }) {
   const [showPopular, setShowPopular] = useState(true);
   const [openDropdown, setOpenDropdown] = useState(false);
   const dropdownRef = useRef();
   useOnClickOutside(dropdownRef, () => setOpenDropdown(false));
+  const [shownMovieId, setShownMovieId] = useState(null);
 
   const getPopularMovieCoverPath = (fileName) => `${process.env.NEXT_PUBLIC_TMDB_IMAGE_HOST_URL}/t/p/w500${fileName}`;
   const getMyMovieCoverPath = (fileName) => {
@@ -17,6 +19,12 @@ function PopularMovies({ popularMovies, myMovies }) {
   }
 
   const getYear = (movie) => movie.release_date?.split('-')[0];
+  const showMovieDetail = (movieId) => shownMovieId === movieId;
+
+  const handleSelectDropdownOption = (showPopular) => {
+    setShownMovieId(null);
+    setShowPopular(showPopular);
+  }  
 
   return (
     <div className={styles.trendingMoviesContainer}>
@@ -31,17 +39,17 @@ function PopularMovies({ popularMovies, myMovies }) {
         {openDropdown && <ul className={styles.dropdown}>
           <i className={styles.arrowUp} />
           <li className={cn(styles.dropdownItem, { [styles.selected]: showPopular })}>
-            <button type="button" className={styles.dropdownItemButton} onClick={() => setShowPopular(true)}>Populares</button>
+            <button type="button" className={styles.dropdownItemButton} onClick={() => handleSelectDropdownOption(true)}>Populares</button>
           </li>
           <li className={cn(styles.dropdownItem, { [styles.selected]: !showPopular })}>
-            <button type="button" className={styles.dropdownItemButton} onClick={() => setShowPopular(false)}>Mis películas</button>
+            <button type="button" className={styles.dropdownItemButton} onClick={() => handleSelectDropdownOption(false)}>Mis películas</button>
           </li>
         </ul>}
       </div>
       <div className={styles.movieListContainer}>
         <div className={cn(styles.popularMoviesContainer, { [styles.visible]: showPopular })}>
           {popularMovies.map((movie) => (
-            <div key={movie.id} className={styles.moviePreviewContainer}>
+            <div key={movie.id} className={cn(styles.moviePreviewContainer, { [styles.showInfo]: showMovieDetail(movie.id) })} onClick={() => setShownMovieId(movie.id)}>
               <Image src={getPopularMovieCoverPath(movie.poster_path)} alt={movie.title} className={styles.movieCover} layout="fill" objectFit="cover" />
               <h3 className={styles.movieTitle}>{movie.title}</h3>
               <div className={styles.hoveredInformation}>
@@ -58,7 +66,7 @@ function PopularMovies({ popularMovies, myMovies }) {
         </div>
         <div className={cn(styles.myMoviesContainer, { [styles.visible]: !showPopular })}>
           {myMovies.map((movie) => (
-            <div key={movie.id} className={styles.moviePreviewContainer}>
+            <div key={movie.id} className={cn(styles.moviePreviewContainer, { [styles.showInfo]: showMovieDetail(movie.id) })} onClick={() => setShownMovieId(movie.id)}>
               <Image src={getMyMovieCoverPath(movie.poster_path)} unoptimized alt={movie.title} className={styles.movieCover} layout="fill" objectFit="cover" />
               <h3 className={styles.movieTitle}>{movie.title}</h3>
               <div className={styles.hoveredInformation}>
